269a4d853b2d34a2f680b7ca122cf11940764657
Barzai of Ulthar <barzai.of.ulthar@gmail.com>
2025-04-01 15:13:47 -0400

    Fix the return type of our `main` to the rust standard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use toyql::_main;
 
 // THIS FUNCTION MUST BE VACUOUS:  Rust-test cannot reach it.
-fn main() {
-    crate::_main();
+fn main() -> Result<(), i32> {
+    crate::_main()
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -2,5 +2,5 @@ use toyql::_main;
 
 /// Smoke test:  Does our `main` actually run?
 fn main() {
-    crate::_main();
+    crate::_main().unwrap();
 }
