2d21b6588ea3c94124d814bfd6f8d22c4af83126
Barzai <barzai.of.ulthar@gmail.com>
2025-05-02 12:15:39 -0400

    Check for leakage of platform data (#10)

--- validate.sh
+++ validate.sh
@@ -9,5 +9,13 @@ for f in ./tests/*.py ; do
     $f
 done
 
+# We should not have leaked development platform information into the repo.
+if [! -z `git grep $USER`] ; then
+    fail "Username is present in repo!"
+fi
+if [! -z `git grep $(hostname)`] ; then
+    fail "hostname is present in repo!"
+fi
+
 # Once system tests are complete, ensure that the remaining tests pass.
 ./precheck.sh
